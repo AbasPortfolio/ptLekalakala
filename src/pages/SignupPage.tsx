2d9befe617e049f940a6fc1e82750a6b61cc36ac
Abas Muzi <106030034+AbasPortfolio@users.noreply.github.com>
2025-12-09 15:45:40 +0200

    Update SignupPage.tsx

--- src/pages/SignupPage.tsx
+++ src/pages/SignupPage.tsx
@@ -36,7 +36,7 @@ function SignupPage() {
         const sanitizedPassword = sanitizeInput(password);
 
         try {
-            const response = await axios.post('http://localhost:5050/record/register', {
+            const response = await axios.post('https://backrecord.onrender.com/record/register', {
                 email: sanitizedEmail,
                 username: sanitizedUsername,
                 password: sanitizedPassword,
@@ -144,3 +144,4 @@ function SignupPage() {
 }
 
 export default SignupPage;
+
