86b61b7a6c4e72ed9cf9a11fe0b12d774f35670e
Abas Muzi <106030034+AbasPortfolio@users.noreply.github.com>
2025-12-09 15:33:48 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -9,8 +9,8 @@ import CreatePage from './pages/CreatePage';
 
 import PatientsPage from './pages/PatientsPage';
 import NotFound from './pages/NotFound';
-import SettingsItemPage from './organisms/SettingsItemPage';
-import Profile from "./pages/Profile.tsx";
+
+
 import EditProfile from "./organisms/EditProfile";
 import UserItemPage from './organisms/UserItemPage';
 import ProtectedRoute from "./pages/ProtectedRoute";
@@ -28,14 +28,13 @@ const App: React.FC = () => {
                     <Route path="*" element={<NotFound />} />
 
                     {/* Protected routes */}
-                    <Route path="/user" element={<ProtectedRoute element={Profile} />} />
+      
                     <Route path="/home" element={<ProtectedRoute element={HomePage} />} />
                     <Route path="/settings" element={<ProtectedRoute element={SettingsPage} />} />
                     <Route path="/edit" element={<ProtectedRoute element={EditProfile} />} />
                     <Route path="/create" element={<ProtectedRoute element={CreatePage} />} />
                     <Route path="/patients" element={<ProtectedRoute element={PatientsPage} />} />
 
-                    <Route path="/settings/:id" element={<ProtectedRoute element={SettingsItemPage} />} />
                     <Route path="/patients/:id" element={<ProtectedRoute element={UserItemPage} />} />
                     <Route path="/profile/editProfile" element={<ProtectedRoute element={EditProfile} />} />
                 </Routes>
@@ -43,5 +42,6 @@ const App: React.FC = () => {
         </UserProvider>
     );
 }
+
 
 export default App;
