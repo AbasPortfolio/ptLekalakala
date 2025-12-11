5660ccac46d81a50ea5397b64609a88c4269ee1c
Abas Muzi <106030034+AbasPortfolio@users.noreply.github.com>
2025-12-11 12:20:15 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -9,17 +9,17 @@ import CreatePage from './pages/CreatePage';
 
 import PatientsPage from './pages/PatientsPage';
 import NotFound from './pages/NotFound';
-
-
+import SettingsItemPage from './organisms/SettingsItemPage';
+import Profile from "./pages/Profile.tsx";
 import EditProfile from "./organisms/EditProfile";
 import UserItemPage from './organisms/UserItemPage';
 import ProtectedRoute from "./pages/ProtectedRoute";
- // Import UserProvider
+import { UserProvider } from './context/UserContext'; // Import UserProvider
 
 const App: React.FC = () => {
     const location = useLocation();
     return (
-        
+        <UserProvider>
             <div className={`${location.pathname === '/' ? 'Loading-background' : 'App'}`}>
                 <Routes>
                     <Route path="/" element={<LoadingPage />} />
@@ -28,23 +28,20 @@ const App: React.FC = () => {
                     <Route path="*" element={<NotFound />} />
 
                     {/* Protected routes */}
-                    
+                    <Route path="/user" element={<ProtectedRoute element={Profile} />} />
                     <Route path="/home" element={<ProtectedRoute element={HomePage} />} />
                     <Route path="/settings" element={<ProtectedRoute element={SettingsPage} />} />
                     <Route path="/edit" element={<ProtectedRoute element={EditProfile} />} />
-                    <Route path="/create" element={<CreatePage />} />
+                    <Route path="/create" element={<ProtectedRoute element={CreatePage} />} />
                     <Route path="/patients" element={<ProtectedRoute element={PatientsPage} />} />
 
-                    
+                    <Route path="/settings/:id" element={<ProtectedRoute element={SettingsItemPage} />} />
                     <Route path="/patients/:id" element={<ProtectedRoute element={UserItemPage} />} />
                     <Route path="/profile/editProfile" element={<ProtectedRoute element={EditProfile} />} />
                 </Routes>
             </div>
-        
+        </UserProvider>
     );
 }
 
 export default App;
-
-
-
